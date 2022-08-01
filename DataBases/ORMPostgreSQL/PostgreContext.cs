852ae002ecfebe513ebdcf0e35dc528a272a20df
Gugnin Dmitry <76938322+sityrax@users.noreply.github.com>
2022-08-02 01:12:07 +0500

    Add logging
    Add logging to a text file and debug.

--- DataBases/ORMPostgreSQL/PostgreContext.cs
+++ DataBases/ORMPostgreSQL/PostgreContext.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿#define LOGTEST
+
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
-using Services;
+using System.IO;
 using System;
+using System.Diagnostics;
 
 namespace ORMPostgreSQL
 {
-    public class PostgreContext<T> : DbContext where T : class, IProduct<T>
+    public class PostgreContext<T> : DbContext where T : class
     {
         public DbSet<T> products { get; set; } = null!;
+
+        #region Logging
+#if RELEASE
+        static int _logStreamsCount;
+        static StreamWriter _logStream;
+#endif
+        #endregion
 
         #region Connection settings
         string Host { get; }
@@ -17,7 +27,6 @@ namespace ORMPostgreSQL
         string Password { get; }
         #endregion
 
-        ///<exception cref="Exception"></exception>
         public PostgreContext(string host = "localhost", 
                               string port = "5432", 
                               string dataBase = "master", 
@@ -40,21 +49,51 @@ namespace ORMPostgreSQL
                                      $"Database={DataBase};" +
                                      $"Username={Username};" +
                                      $"Password={Password}");
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
+#region Logging
+#if RELEASE
+            if (_logStreamsCount == 0)
+                _logStream = new StreamWriter(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Errorlog.txt"), append: true);
+            _logStreamsCount++;
+                optionsBuilder.LogTo(_logStream.WriteLine)
+                              .EnableSensitiveDataLogging();
+#elif LOGTEST
+            optionsBuilder.LogTo(x => Debug.WriteLine(x))
+                          .EnableSensitiveDataLogging();
+#endif
+#endregion
         }
 
         public override void Dispose()
         {
             base.Dispose();
+#region Logging
+#if RELEASE
+            _logStream?.WriteLine("Logstream is closed.\n\n");
+            if (_logStreamsCount == 1)
+            {
+                _logStream?.Dispose();
+                _logStreamsCount--;
+            }
+            else
+                _logStreamsCount--;
+#endif
+#endregion
         }
 
         public override async ValueTask DisposeAsync()
         {
             await base.DisposeAsync();
+#region Logging
+#if RELEASE
+            if (_logStreamsCount == 1)
+            {
+                await _logStream.DisposeAsync();
+                _logStreamsCount--;
+            }
+            else
+                _logStreamsCount--;
+#endif
+#endregion
         }
     }
 }
