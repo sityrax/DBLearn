49b9d404e4de007ad031ade0737d25b5ff4fbb3d
Gugnin Dmitry <76938322+sityrax@users.noreply.github.com>
2022-08-18 22:22:18 +0500

    Add Lazy, Eager and Explicit loading test class

---
+++ DataBases/SQLiteComplex/SQLiteContextComplex.cs
@@ -0,0 +1,127 @@
+﻿#define VARIANT
+#define VARIANT2
+#define OWNED
+#define USELAZYLOADING // предполагает неявную автоматическую загрузку связанных данных при обращении к навигационному свойству (такое свойство не представляют непосредственно конкретное поле таблицы, а используются для доступа к данным, связанным с выбранной записью таблице).
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+using System;
+
+namespace ORMPostgreSQL
+{
+    public class SQLiteContextComplex : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Company> Companies { get; set; } = null!;
+        public string DBFileName { get; private set; } = "users";
+
+        public SQLiteContextComplex() 
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+#if USELAZYLOADING
+                .UseLazyLoadingProxies()
+#endif
+                .UseSqlite($"Data Source = {DBFileName}.db");
+            optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+        }
+
+        // тестируем FluentApi.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<User>().HasKey(u => u.AltId); // можно не указывать т.к. по умолчанию Id или [Имя_класса]Id, например, UserId.
+            modelBuilder.Entity<User>().HasOne(u => u.Company)   // устанавливаем тип связи (один ко многим в данном случае).
+                                       .WithMany(c => c.Users)
+                                       .HasForeignKey(ui => ui.CompanyId);  // назначаем внешний ключ.
+            modelBuilder.Entity<User>().Property(p => p.Age)
+                                       .IsRequired();   // добавляем требование NOT NULL к свойству (атрибуту) Age.
+            modelBuilder.Entity<User>().Ignore(i => i.toIgnore);    // игнорируем свойство toIgnore (не добавляем в таблицу).
+#if VARIANT2
+            modelBuilder.Entity<User>().HasOne(u => u.FullName)
+                                       .WithOne(ui => ui.User)
+                                       .HasForeignKey<UserInfo>(ui => ui.Id);   // определяем связь User с UserInfo по внешнему ключу для последующего добавления данных навигационого свойства в единую таблицу users.
+            modelBuilder.Entity<User>().ToTable("users");   // добавялем свойства типа User в таблицу users.
+            modelBuilder.Entity<UserInfo>().ToTable("users");   // добавляем свойства типа UserInfo в таблицу users.
+#endif
+#if (VARIANT || VARIANT2) && !OWNED
+            modelBuilder.Entity<UserInfo>().OwnsOne(u => u.Patronymic);
+#endif
+#if (!VARIANT && !VARIANT2) && !OWNED
+            // Главной сущностью здесь является класс User, который содержит объект класса UserInfo. Класс UserInfo, в свою очередь, также содержит объекты еще одного класса - Patronymic. OwnsOne включает объекты типов UserInfo и Patronymic в результирующую таблицу User, для этого этим типам не нужен свое поле id.
+            modelBuilder.Entity<User>().OwnsOne(us => us.FullName, u => 
+            {
+                u.OwnsOne(c => c.Patronymic);
+            });
+#endif
+        }
+    }
+
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+#if USELAZYLOADING
+        public virtual List<User> Users { get; set; } = new();
+#else
+        public List<User> Users { get; set; } = new();
+#endif
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+#if USELAZYLOADING
+        public virtual UserInfo FullName { get; set; }
+#else
+        public UserInfo FullName { get; set; }
+#endif
+        public int Age { get; set; }
+        public int CompanyId { get; set; }
+        public bool toIgnore { get; set; }
+#if USELAZYLOADING
+        public virtual Company Company { get; set; }
+#else
+        public Company Company { get; set; }
+#endif
+    }
+
+    public class UserInfo
+    {
+#if VARIANT || (!VARIANT && !VARIANT2 && OWNED) || (!VARIANT && VARIANT2)
+        public int Id { get; set; }
+#endif
+#if VARIANT2
+#if USELAZYLOADING
+        public virtual User User { get; set; }
+#else
+        public User User { get; set; }
+#endif
+#endif
+#if !VARIANT && VARIANT2
+        public int UserId { get; set; }
+#endif
+        public string Name { get; set; }
+        public string Surname { get; set; }
+#if USELAZYLOADING
+        public virtual Patronymic Patronymic { get; set; }
+#else
+        public Patronymic Patronymic { get; set; }
+#endif
+    }
+
+#if OWNED
+    [Owned]
+#endif
+    public class Patronymic
+    {
+        public int Key { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ DataBases/SQLiteComplexTests/LiteContextComplexTests.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System;
+
+namespace ORMPostgreSQL.IntegrationTests
+{
+    [TestClass()]
+    public class LiteContextComplexTests
+    {
+        static Company[] companies;
+        static User[] users;
+
+        [ClassInitialize]
+        public static void TestInit(TestContext testContext)
+        {
+            //Arrange
+            Patronymic i       = new Patronymic { Key = 1, Name = "I." };
+            Patronymic a       = new Patronymic { Key = 2, Name = "A." };  // нельзя добавлять один и тот же объект разным владельцам.
+            Patronymic e       = new Patronymic { Key = 1, Name = "E." };
+            Patronymic unknown = new Patronymic { Key = 1, Name = "unknown" };
+
+            Company allsafe    = new Company { Name = "Allsafe Cybersecurity" };
+            Company metacortex = new Company { Name = "Metacortex" };
+            Company merlaut    = new Company { Name = "The Merlaut" };
+            companies = new Company[] { allsafe, metacortex, merlaut };
+
+            #region Users add
+            UserInfo genaInfo   = new UserInfo { Name = "Gena",    Surname = "Hacker",   Patronymic = i };
+            UserInfo tomasInfo  = new UserInfo { Name = "Tomas",   Surname = "Anderson", Patronymic = a };
+            UserInfo elliotInfo = new UserInfo { Name = "Elliot",  Surname = "Alderson", Patronymic = e };
+            UserInfo aidenInfo  = new UserInfo { Name = "Aiden",   Surname = "Pearce",   Patronymic = unknown };
+
+            User gena   = new User { FullName = genaInfo,   Company = merlaut };
+            User tomas  = new User { FullName = tomasInfo,  Company = metacortex };
+            User elliot = new User { FullName = elliotInfo, Company = allsafe };
+            User aiden  = new User { FullName = aidenInfo,  Company = merlaut };
+            users = new User[] { gena, tomas, elliot, aiden };
+            #endregion
+
+            using (SQLiteContextComplex db = new SQLiteContextComplex())
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+                db.Companies.AddRange(companies);
+                db.Users.AddRange(users);
+                db.SaveChanges();
+            }
+        }
+
+        [TestMethod()]
+        public void EagerLoadingTest()
+        {
+            using(SQLiteContextComplex db = new SQLiteContextComplex())
+            { 
+                //Act
+                var result = db.Companies.Include(c => c.Users).ThenInclude(u => u.FullName).ToList();
+
+                //Assert
+                for (int i = 0; i < result.Count; i++)
+                {
+                    for (int j = 0; j < result[i].Users.Count; j++)
+                    {
+                        for (int k = 0; k < users.Length; k++)
+                        {
+                        if (users[k].Id                       != result[i].Users[j].Id ||
+                            users[k].FullName.Name            != result[i].Users[j].FullName.Name ||
+                            users[k].FullName.Surname         != result[i].Users[j].FullName.Surname ||
+                            users[k].FullName.Patronymic.Name != result[i].Users[j].FullName.Patronymic.Name ||
+                            users[k].Company.Name             != result[i].Users[j].Company.Name)
+                        {
+                            if (users.Length - k == 1)
+                                throw new Exception();
+                        }
+                        else
+                            break;
+                        }
+                    }
+                }
+                Assert.IsTrue(true);
+            }
+        }
+
+        [TestMethod()]
+        public void ExplicitLoadingTest()
+        {
+            using(SQLiteContextComplex db = new SQLiteContextComplex())
+            {
+                var usersCollection = db.Users.AsEnumerable();
+                db.Users.Load();  // метод Include не используется
+                foreach (var item in usersCollection)
+                {
+                    db.Entry(item).Reference(c => c.Company).Load();
+                    db.Entry(item).Reference(c => c.FullName).Load();
+                }
+
+                //Assert
+                for (int i = 0; i < usersCollection.Count(); i++)
+                {
+                    for (int j = 0; j < users.Length; j++)
+                    {
+                        if (usersCollection.ElementAt(i).Id                       != users[j].Id ||
+                            usersCollection.ElementAt(i).FullName.Name            != users[j].FullName.Name ||
+                            usersCollection.ElementAt(i).FullName.Surname         != users[j].FullName.Surname ||
+                            usersCollection.ElementAt(i).FullName.Patronymic.Name != users[j].FullName.Patronymic.Name ||
+                            usersCollection.ElementAt(i).Company.Name             != users[j].Company.Name ||
+                            usersCollection.ElementAt(i).Company.Id               != users[j].Company.Id)
+                        {
+                            if (usersCollection.Count() - j == 1)
+                                throw new Exception();
+                        }
+                        else
+                            break;
+                    }
+                }
+                Assert.IsTrue(true);
+            }
+        }
+
+        [TestMethod()]
+        public void LazyLoadingCompaniesTest()
+        {
+            using (SQLiteContextComplex db = new SQLiteContextComplex())
+            {
+                var companies = db.Companies.ToList();
+
+                //Assert
+                for (int i = 0; i < companies.Count(); i++)
+                {
+                    if(companies[i].Users.Count > 0)
+                    for (int j = 0; j < companies[i].Users.Count; j++)
+                    {
+                        if (companies[i].Users[j].Id                       != users[i].Id ||
+                            companies[i].Users[j].Age                      != users[i].Age ||
+                            companies[i].Users[j].FullName.Name            != users[i].FullName.Name ||
+                            companies[i].Users[j].FullName.Surname         != users[i].FullName.Surname ||
+                            companies[i].Users[j].FullName.Patronymic.Name != users[i].FullName.Patronymic.Name ||
+                            companies[i].Users[j].Company.Name             != users[i].Company.Name ||
+                            companies[i].Users[j].Company.Id               != users[i].Company.Id)
+                        {
+                            if (users.Length - j == 1)
+                                throw new Exception($"Some properties of User {j} in Company {i} is wrong.");
+                        }
+                        else
+                            break;
+                    }
+                    else
+                        throw new Exception($"Users collection in company {i} is empty.");
+                }
+                Assert.IsTrue(true);
+            }
+        }
+
+        [TestMethod()]
+        public void LazyLoadingUsersTest()
+        {
+            using (SQLiteContextComplex db = new SQLiteContextComplex())
+            {
+                var usersCollection = db.Users.ToList();
+
+                //Assert
+                for (int i = 0; i < companies.Count(); i++)
+                {
+                    for (int j = 0; j < users.Length; j++)
+                    {
+                        if (usersCollection[i].Id                       != users[j].Id ||
+                            usersCollection[i].Age                      != users[j].Age ||
+                            usersCollection[i].FullName.Name            != users[j].FullName.Name ||
+                            usersCollection[i].FullName.Surname         != users[j].FullName.Surname ||
+                            usersCollection[i].FullName.Patronymic.Name != users[j].FullName.Patronymic.Name ||
+                            usersCollection[i].Company.Name             != users[j].Company.Name ||
+                            usersCollection[i].Company.Id               != users[j].Company.Id)
+                        {
+                            if (users.Length - j == 1)
+                                throw new Exception();
+                        }
+                        else
+                            break;
+                    }
+                }
+                Assert.IsTrue(true);
+            }
+        }
+
+        [ClassCleanup]
+        public static void Delete()
+        {
+            using (SQLiteContextComplex db = new SQLiteContextComplex())
+                db.Database.EnsureDeleted();
+        }
+    }
+}
